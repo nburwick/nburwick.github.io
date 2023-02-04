006599ac9dadcbc7a9a9c6bed259642aecb6e245
nburwick <121465238+nburwick@users.noreply.github.com>
2023-02-03 22:09:58 -0700

    Fixed typos and list comprehension in main.py

--- Python-challenge/main.py
+++ Python-challenge/main.py
@@ -21,10 +21,10 @@ def pybank(file_in, file_out):
     with open(file_in) as pybank:
         pybank_csv_reader = csv.reader(pybank,delimiter=',')
         
-         #store header for finding index of column headers since we know them
+        #store header for finding index of column headers since we know them
         header = next(pybank_csv_reader)
-        print(header)
-         #read and store rows into memory
+        
+        #read and store rows into memory
         lines = []
         for row in pybank_csv_reader:
             lines.append(row)
@@ -38,6 +38,8 @@ def pybank(file_in, file_out):
     #instanciate empty lists for moving average
     month_list, p_and_l = map(list, zip(*lines))
     change_list = []
+    #convert p_and_l to floats
+    p_and_l = list(map(float,p_and_l))
     
     #get total months:
     total_months = len(month_list)
@@ -54,9 +56,9 @@ def pybank(file_in, file_out):
     avg_change = (sum(change_list)/len(change_list))
     
     #create dict of changes
-    change_months = month_list.pop(0)
+    change_months = month_list[1:]
     change_dict = dict(zip(change_months, change_list))
-    
+
     #find greatest increase/decrease keys and values
     increase_month = max(change_dict,key=change_dict.get)
     increase_value = change_dict.get(increase_month)
@@ -65,14 +67,14 @@ def pybank(file_in, file_out):
         
     #define writer to write analysis results from pybank
     with open(file_out,'w',newline="") as writer:
-        writer.write("Financial Analysis\n")
+        writer.write("\nFinancial Analysis\n")
         writer.write("-----------------------------------\n")
         writer.write(f"Total Months: {total_months}\n")
         writer.write(f"Total: ${'{:,.2f}'.format(total_net)}\n")
         writer.write(f"Average Change: ${'{:,.2f}'.format(avg_change)}\n")
         writer.write(f"Greatest Increase in Profits: {increase_month} (${'{:,.2f}'.format(increase_value)})\n")
         writer.write(f"Greatest Decrease in Profits: {decrease_month} (${'{:,.2f}'.format(decrease_value)})\n")
-        writer.write("-----------------------------------\n")
+        writer.write("-----------------------------------")
 
 #define a function for threading PyPoll
 def pypoll(file_in, file_out):
@@ -83,7 +85,7 @@ def pypoll(file_in, file_out):
         
         #store header for finding index of column headers since we know them
         header = next(pypoll_csv_reader)
-        print(header)
+       
         #Read and store rows into memory
         lines = []
         for row in pypoll_csv_reader:
@@ -109,14 +111,14 @@ def pypoll(file_in, file_out):
     
     #define writer to write analysis results from pypoll    
     with open(file_out,'w') as writer:
-        writer.write("Election Results\n")
+        writer.write("\nElection Results\n")
         writer.write("-----------------------------------\n")
-        writer.write(f"Total Votes: {votes_cast_total}")
+        writer.write(f"Total Votes: {votes_cast_total}\n")
         writer.write("-----------------------------------\n")
         for candidate, votes in candidate_dict.items():
             writer.write(f"{candidate}: {round(((votes/votes_cast_total)*100),ndigits=2)}% ({'{:,.0f}'.format(votes)})\n")
         writer.write("-----------------------------------\n")
-        writer.write(f"Winner: {winner}")
+        writer.write(f"Winner: {winner}\n")
         writer.write("-----------------------------------")
         
     
