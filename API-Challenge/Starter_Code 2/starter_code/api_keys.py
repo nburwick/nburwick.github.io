f9407bcea564b76d32fa3c8bf29ed9cb42c8bf9f
nburwick <121465238+nburwick@users.noreply.github.com>
2023-03-02 16:10:31 -0700

    add .gitignore

--- API-Challenge/Starter_Code 2/starter_code/api_keys.py
+++ API-Challenge/Starter_Code 2/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6da5b6007bbc74258f8e8d621c368491"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "7ea854e9af1d43fc94ff8f3913e53521"
