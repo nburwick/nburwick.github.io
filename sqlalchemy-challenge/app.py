49bc31666f81d99ba454a7f019e6d72ee2b0e5fe
nburwick <121465238+nburwick@users.noreply.github.com>
2023-03-28 21:23:28 -0700

    Initialized Flask App

--- sqlalchemy-challenge/app.py
+++ sqlalchemy-challenge/app.py
@@ -1,30 +1,242 @@
 # Import the dependencies.
+from flask import Flask, jsonify
+import datetime as dt
 
-
+# Import sqlalchemy modules.
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, text, inspect
 
 #################################################
 # Database Setup
 #################################################
+# Create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+# Create Inspector for Exploration
+inspector = inspect(engine)
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
+Base = automap_base()
 
-# reflect the tables
-
+# Reflect the tables
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+# Instanciate Flask App
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+# Define Home Page
+@app.route("/")
+def home():
+    return (
+    f"<h2>Welcome to the Hawaii Climate App</h2><br/>"
+    f"Available routes are:<br/>"
+    f"<ul><li>Precipitation - <code>/api/v1.0/precipitation</code></li></ul>"
+    f"<pre>         Returns a json containing a year (12 months) data from last data point's date in the database. (2016-08-23 to 2017-08-23)<br>"
+    f"<ul><li>Stations - <code>/api/v1.0/stations</code></li></ul>"
+    f"<pre>         Returns a json of 'station_id', 'station_name', 'lat', and 'lng' <br>"
+    f"<ul><li>Temperature Observations - <code>/api/v1.0/tobs</code></li></ul>"
+    f"<pre>         Returns a json temperature observations from the 'Most Active Station' in the database.<br>"
+    f"<ul><li>Calculated Temperatures (Single Date) - <code>/api/v1.0/start</code></li></ul>"
+    f"<ul><li>Calculated Temperatures (Dual Dates) - <code>/api/v1.0/start/end</code></li></ul>"
+    f""
+    f"The <b>Calculated Temperatures</b> route will return the minimum, maximum, and average temperatures of the date range given."
+    f"<br>If a single date is given, temperatures will be calculated for all dates equal to or greater than the date given."
+    f"<br>If two dates are given, temperatures will be calculated for all dates between the two given, inclusively."
+    f"<br>To use these routes, replace <code>start</code> and <code>end</code> with dates in YYYY-MM-DD format.")
+
+#################################################
+
+# Define API JSON Response - precipitation
+@app.route("/api/v1.0/precipitation")
+def prcp():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Find the most recent date in the data set.
+    recent_date = session.query(func.max(measurement.date))[0][0]
+
+    # Calculate the date one year from the last date in data set.
+    recent_date = dt.datetime.strptime(recent_date, '%Y-%m-%d')
+    query_date = dt.date(recent_date.year - 1, recent_date.month, recent_date.day)
+
+    # Design a query to retrieve the last 12 months of precipitation data and plot the results.
+    query = f"""
+                SELECT date, prcp
+                FROM measurement
+                WHERE date >= '{str(query_date)}' AND prcp NOT NULL
+                GROUP BY station, date
+                ORDER BY date
+                """
+    # Execute Query in sqlite db.
+    results = session.execute(text(query)).all()
+
+    # Create a list of dictionaries to hold rows of data
+    prcp_list = [{'date': date, 'prcp' : prcp} for date, prcp in results]
+    
+    # Close Session
+    session.close()
+    
+    # Return API Response Data
+    return jsonify(prcp_list)
+
+#################################################
+   
+# Define API JSON Response - station
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Design Query for Reponse Results - Station ID, Station Name, Lat, Lng
+    select = [station.station, station.name, station.latitude, station.longitude]
+    results = session.query(*select).all()
+    
+    # Create list of dictionaries to hold rows of data
+    stations_list = [{'station_id' : id,
+                      'station_name' : name,
+                      'lat' : lat, 
+                      'lng' : lng} for id, name, lat, lng in results]
+    
+    # Close Session
+    session.close()
+    
+    # Return API Response Data
+    return jsonify(stations_list)
+
+#################################################
+
+# Define API JSON Response - Temperature Observations for Most Active Station
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+    
+    # Design Query to get Most Active Station
+    station = session.execute("""
+            SELECT station, COUNT(station) AS Count
+              FROM measurement
+             GROUP BY station
+             ORDER BY Count DESC
+             LIMIT 1
+          """).first()[0]
+
+    # Find the most recent date in the data set.
+    recent_date = session.query(func.max(measurement.date))[0][0]
+
+    # Calculate the date one year from the last date in data set.
+    recent_date = dt.datetime.strptime(recent_date, '%Y-%m-%d')
+    query_date = dt.date(recent_date.year - 1, recent_date.month, recent_date.day)
+    
+    # Design Query for Response Data
+    query = f"""
+             SELECT date, tobs, station
+               FROM measurement
+              WHERE date >= '{str(query_date)}' and station = '{station}'
+              ORDER BY date   
+             """
+    # Execute Query
+    results = session.execute(text(query)).all()
+    
+    # Create list of dictionaries to hold rows of data
+    tobs_list = [{'date': date, 'temp_observation': tobs, 'station_id' : station } for date, tobs, station in results]
+    
+    # Close Session
+    session.close()
+        
+    # Return API Response Data
+    return jsonify(tobs_list)
+
+#################################################
+    
+# Define Single Date Calculated Temperature List
+@app.route("/api/v1.0/<start>")
+def single_date(start):
+    # Get Start Date
+    start_date = str(start)
+    
+    try:
+        check_date = dt.datetime.strptime(start_date, '%Y-%m-%d')
+    except:
+        return jsonify({'error': "An incorrect date format was entered. Please use a YYYY-MM-DD and try again"})
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Design Query
+    query = f"""
+                SELECT date, MIN(tobs), MAX(tobs), ROUND(AVG(tobs), 1)
+                FROM measurement
+                WHERE date >= '{start_date}'
+                GROUP BY date
+                ORDER BY date
+             """
+    # Execute Query
+    results = session.execute(text(query)).all()
+    
+    # Create list of dictionaries to hold rows of data
+    results_list = [{'date': date, 'TMIN' : min, 'TMAX': max, 'TAVG' : avg} for date, min, max, avg in results]
+
+    # Close Session
+    session.close()
+    
+    # Return API Response Data
+    return jsonify(results_list)
+
+#################################################    
+
+# Define Dual Date Calculated Temperature List
+@app.route("/api/v1.0/<start>/<end>")
+def dual_date(start, end):
+    # Get Start and End Date
+    start_date = str(start)
+    end_date = str(end)
+    date_range = [start_date, end_date]
+    
+    try:
+        for date in date_range:
+            check_date = dt.datetime.strptime(date, '%Y-%m-%d')
+    except:
+        return jsonify({'error': "An incorrect date format was entered. Please use a YYYY-MM-DD and try again"})
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Design Query
+    query = f"""
+                SELECT date, MIN(tobs), MAX(tobs), ROUND(AVG(tobs), 1)
+                FROM measurement
+                WHERE date BETWEEN '{start_date}' AND '{end_date}'
+                GROUP BY date
+                ORDER BY date
+             """
+    # Execute Query
+    results = session.execute(text(query)).all()
+    
+    # Create list of dictionaries to hold rows of data
+    results_list = [{'date': date, 'TMIN' : min, 'TMAX': max, 'TAVG' : avg} for date, min, max, avg in results]
+
+    # Close Session
+    session.close()
+    
+    # Return API Response Data
+    return jsonify(results_list)
+
+#################################################
+
+# Initiate App
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
